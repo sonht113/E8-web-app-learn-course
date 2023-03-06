0644d6f9e14c31a9705f87b6e6866d9ad4700a4a
trongson <trongson18101999@gmail.com>
2023-03-06 20:45:50 +0700

    [06-03-2023] create banner at homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,58 @@ import React, { ReactElement } from 'react';
 
 import { NextPageWithLayout } from '../types/layout.type';
 import DefaultLayout from 'layouts/defaultLayout';
+import Banner from '@/components/Banner';
+import { BannerType } from 'types/banner.type';
+import Course from '@/components/Course';
+import { Container, Flex } from '@chakra-ui/react';
+
+const bannerItems: BannerType[] = [
+  {
+    id: 1,
+    image:
+      'https://files.fullstack.edu.vn/f8-prod/banners/20/6308a6bf603a4.png',
+    title: 'KHOÁ HỌC HTML CSS PRO',
+    description:
+      'Đây là khoá học đầy đủ và chi tiết nhất mà bạn có thể tìm trên internet!',
+    button: 'Tìm hiểu thêm',
+    colorFrom: 'rgb(104, 40, 250)',
+    colorTo: 'rgb(255, 186, 164)',
+  },
+  {
+    id: 2,
+    image:
+      'https://files.fullstack.edu.vn/f8-prod/banners/20/6308a6bf603a4.png',
+    title: 'KHOÁ HỌC HTML CSS PRO',
+    description:
+      'Đây là khoá học đầy đủ và chi tiết nhất mà bạn có thể tìm trên internet!',
+    button: 'Tìm hiểu thêm',
+    colorFrom: 'rgb(104, 40, 250)',
+    colorTo: 'rgb(255, 186, 164)',
+  },
+  {
+    id: 3,
+    image:
+      'https://files.fullstack.edu.vn/f8-prod/banners/20/6308a6bf603a4.png',
+    title: 'KHOÁ HỌC HTML CSS PRO',
+    description:
+      'Đây là khoá học đầy đủ và chi tiết nhất mà bạn có thể tìm trên internet!',
+    button: 'Tìm hiểu thêm',
+    colorFrom: 'rgb(104, 40, 250)',
+    colorTo: 'rgb(255, 186, 164)',
+  },
+];
 
 const Home: NextPageWithLayout = () => {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <Container className="home">
+      <Banner items={bannerItems} />
+      <Flex alignItems={'center'} gap={5}>
+        {[1, 2, 3, 5].map(() => (
+          <Course />
+        ))}
+      </Flex>
+    </Container>
+  );
 };
 
 Home.getLayout = function getLayout(page: ReactElement) {
---
+++ types/banner.type.ts
@@ -0,0 +1,9 @@
+export type BannerType = {
+  id?: string | number;
+  image: string;
+  title: string;
+  description: string;
+  button: string;
+  colorFrom: string;
+  colorTo: string;
+};
