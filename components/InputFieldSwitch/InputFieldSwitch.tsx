a3ba8ce023fc9b702a0c0939eb8fa953c7ed3e62
sonht113 <trongson18101999@gmail.com>
2023-03-24 01:44:05 +0700

    [24-03-2023] update validate form

--- components/InputFieldSwitch/InputFieldSwitch.tsx
+++ components/InputFieldSwitch/InputFieldSwitch.tsx
@@ -1,6 +1,5 @@
 import React, { ReactElement, useState } from 'react';
 import {
-  FormControl,
   Flex,
   FormLabel,
   InputGroup,
@@ -9,11 +8,10 @@ import {
   Text,
   Box,
 } from '@chakra-ui/react';
-import InputField from '../InputField/InputField';
-import { useRouter } from 'next/router';
 
 type PropertiesType = {
   label?: string;
+  name?: string;
   labelAction?: string;
   placeholder?: string;
   type?: string;
@@ -23,16 +21,21 @@ type PropertiesType = {
 type IInputFieldSwitchProps = {
   defaultInput: PropertiesType;
   switchInput: PropertiesType;
+  validate?: any;
+  errors?: any;
+  reset?: any;
 };
 
 const InputFieldSwitch: React.FC<IInputFieldSwitchProps> = ({
   defaultInput,
   switchInput,
+  validate,
+  errors,
+  reset,
 }) => {
   const [isSwitch, setIsSwitch] = useState<boolean>(false);
-  const router = useRouter();
   return (
-    <FormControl my={8}>
+    <Box my={10}>
       <Flex justifyContent={'space-between'}>
         <FormLabel fontSize={'sm'}>
           {isSwitch ? defaultInput.label : switchInput.label}
@@ -40,39 +43,105 @@ const InputFieldSwitch: React.FC<IInputFieldSwitchProps> = ({
         <FormLabel
           cursor={'pointer'}
           fontSize={'sm'}
-          onClick={() => setIsSwitch(!isSwitch)}
+          onClick={() => {
+            reset();
+            setIsSwitch(!isSwitch);
+          }}
         >
           {!isSwitch ? defaultInput.labelAction : switchInput.labelAction}
         </FormLabel>
       </Flex>
-      <InputGroup rounded={'full'}>
-        {defaultInput.icon && isSwitch && (
-          <InputLeftElement pointerEvents="none" children={defaultInput.icon} />
-        )}
-        {switchInput.icon && !isSwitch && (
-          <InputLeftElement pointerEvents="none" children={switchInput.icon} />
-        )}
-        <Input
-          placeholder={
-            isSwitch ? defaultInput.placeholder : switchInput.placeholder
-          }
-          _placeholder={{ fontSize: 'sm' }}
-          bg={'gray.100'}
-          rounded={'full'}
-          type={!switchInput ? defaultInput.type : switchInput.type}
-        />
-      </InputGroup>
-      <InputGroup display={'flex'} flexDirection={'column'}>
-        <Box>
-          {!isSwitch && <InputField type="password" placeholder="Mật khẩu" />}
-        </Box>
-        {!isSwitch && router.pathname === '/register' && (
-          <Text fontSize={'xs'} color={'gray.600'} pl={3}>
-            Gợi ý: Mật khẩu cần có ít nhất 8 kí tự
+      <Box h={'70px'}>
+        <InputGroup rounded={'full'}>
+          {defaultInput.icon && isSwitch && (
+            <InputLeftElement children={defaultInput.icon} />
+          )}
+          {switchInput.icon && !isSwitch && (
+            <InputLeftElement children={switchInput.icon} />
+          )}
+          {!isSwitch && (
+            <Input
+              placeholder={switchInput.placeholder}
+              _placeholder={{ fontSize: 'sm' }}
+              bg={'gray.100'}
+              rounded={'full'}
+              type={switchInput.type}
+              name={switchInput.name}
+              {...validate(switchInput.name, {
+                required: 'Vui lòng nhập email!',
+                pattern: {
+                  value: /[^\s]*@[a-z0-9.-]*/i,
+                  message: 'Email không hợp lệ',
+                },
+              })}
+            />
+          )}
+          {isSwitch && (
+            <Input
+              placeholder={defaultInput.placeholder}
+              _placeholder={{ fontSize: 'sm' }}
+              bg={'gray.100'}
+              rounded={'full'}
+              type={defaultInput.type}
+              name={defaultInput.name}
+              {...validate(defaultInput.name, {
+                required: 'Vui lòng nhập số điện thoại!',
+                pattern: {
+                  value: /^[0-9]+$/,
+                  message: 'Số điện thoại không hợp lệ',
+                },
+                minLength: {
+                  value: 10,
+                  message: 'Số điện thoại không hợp lệ',
+                },
+                maxLength: {
+                  value: 10,
+                  message: 'Số điện thoại không hợp lệ',
+                },
+              })}
+            />
+          )}
+        </InputGroup>
+        {errors.email && !isSwitch && (
+          <Text fontSize="xs" px={2} color={'red'}>
+            {errors.email?.message}
           </Text>
         )}
+        {errors.phone && isSwitch && (
+          <Text fontSize="xs" px={2} color={'red'}>
+            {errors.phone?.message}
+          </Text>
+        )}
+      </Box>
+      <InputGroup display={'flex'} flexDirection={'column'} h={'70px'}>
+        <Box _focus={{ borderColor: 'none' }}>
+          <FormLabel fontSize={'sm'}>Mật khẩu</FormLabel>
+          <Input
+            placeholder={'Mật khẩu'}
+            _placeholder={{ fontSize: 'sm' }}
+            bg={'gray.100'}
+            type={'password'}
+            rounded={'full'}
+            name={'password'}
+            {...validate('password', {
+              required: 'Vui lòng nhập mật khẩu',
+              minLength: {
+                value: 8,
+                message: 'Mật khẩu không hợp lệ',
+              },
+            })}
+          />
+        </Box>
+        {errors.password && (
+          <Text fontSize="xs" px={2} color={'red'}>
+            {errors.password?.message}
+          </Text>
+        )}
+        <Text fontSize={'xs'} color={'gray.600'} pl={2}>
+          Gợi ý: Mật khẩu cần có ít nhất 8 kí tự
+        </Text>
       </InputGroup>
-    </FormControl>
+    </Box>
   );
 };
 
